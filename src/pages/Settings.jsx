d6b9e77de9b08fec2de9c9e98822a9d30a60c12a
DipalLalkiya <dipallalkiya@gmail.com>
2024-02-29 00:00:40 +0530

    updates are done

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -4,14 +4,14 @@ const SettingsPage = () => {
   return (
     <Box p="16">
       <Box>
-        <Text fontSize="lg" pl="35px" mb='5px'>
+        <Text fontSize="lg" pl="35px" mb="5px">
           Enter secret key provided by your organization to access AI powered
           analysis
         </Text>
       </Box>
 
       <Flex display="flex" p="10px" ml="20px">
-        <Input placeholder="Enter your secret key" />
+        <Input placeholder="Enter your secret key" maxWidth="50%" />
         <Button ml="10px">Apply</Button>
       </Flex>
     </Box>
